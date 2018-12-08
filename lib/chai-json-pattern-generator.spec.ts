df962e9e4cc08f226782e421d2ffc2f7ddb40289
Oscar Rieken <oscar.rieken@adp.com>
2018-12-07 19:09:36 -0500

    chore(lint): fixed unsafe any, 🐙

--- lib/chai-json-pattern-generator.spec.ts
+++ lib/chai-json-pattern-generator.spec.ts
@@ -3,42 +3,60 @@ import { expect } from 'chai';
 
 describe('generateJsonPatternFor', () => {
   it('String value schema', () => {
-    class UghString { someString: string; }
-    const generated = generateJsonPatternFor<UghString>({ someString: 'a' });
+    class UghString {
+      someString: string;
+    }
+
+    const generated = generateJsonPatternFor<UghString>({someString: 'a'});
 
     expect(generated).to.equal(`{ "someString": String, ... }`);
   });
 
   it('Boolean value schema', () => {
-    class UghBoolean { someBoolean: boolean; }
-    const generated = generateJsonPatternFor<UghBoolean>({ someBoolean: true });
+    class UghBoolean {
+      someBoolean: boolean;
+    }
+
+    const generated = generateJsonPatternFor<UghBoolean>({someBoolean: true});
 
     expect(generated).to.equal(`{ "someBoolean": Boolean, ... }`);
   });
 
   it('Number value schema', () => {
-    class UghNumber { someNumber: number; }
-    const generated = generateJsonPatternFor<UghNumber>({ someNumber: 123 });
+    class UghNumber {
+      someNumber: number;
+    }
+
+    const generated = generateJsonPatternFor<UghNumber>({someNumber: 123});
 
     expect(generated).to.equal(`{ "someNumber": Number, ... }`);
   });
 
   it('Object value schema', () => {
-    class UghObject { someObject: { someBoolean: boolean }; }
-    const generated = generateJsonPatternFor<UghObject>({ someObject: { someBoolean: true } });
+    class UghObject {
+      someObject: { someBoolean: boolean };
+    }
+
+    const generated = generateJsonPatternFor<UghObject>({someObject: {someBoolean: true}});
 
     expect(generated).to.equal(`{ "someObject": { "someBoolean": Boolean, ... }, ... }`);
   });
 
   it('undefined value schema', () => {
-    class UghUndefined { someUndefined?: ''; }
-    expect(() => { generateJsonPatternFor<UghUndefined>({ someUndefined: undefined }); }).to.throw();
+    class UghUndefined {
+      someUndefined?: '';
+    }
+
+    expect(() => { generateJsonPatternFor<UghUndefined>({someUndefined: undefined}); }).to.throw();
   });
 
   it('gives a helpful error message when property is undefined', () => {
-    class UghUndefined { someUndefined?: ''; }
+    class UghUndefined {
+      someUndefined?: '';
+    }
+
     try {
-      generateJsonPatternFor<UghUndefined>({ someUndefined: undefined });
+      generateJsonPatternFor<UghUndefined>({someUndefined: undefined});
     } catch (e) {
       expect(e.message).to.equal('someUndefined was undefined'); // tslint:disable-line:no-unsafe-any
     }
@@ -50,40 +68,53 @@ describe('generateJsonPatternFor', () => {
       someNumber: number;
       someString: string;
     }
+
     const generated = generateJsonPatternFor<Ugh>({
-      someBoolean: false,
-      someNumber: 123,
-      someString: 'abc'
-    });
+                                                    someBoolean: false,
+                                                    someNumber: 123,
+                                                    someString: 'abc'
+                                                  });
 
     expect(generated).to.equal(`{ "someBoolean": Boolean,\n"someNumber": Number,\n"someString": String, ... }`);
   });
 
   describe('Array', () => {
     it('containing numbers', () => {
-      class UghArray { someArray?: number[]; }
-      const generated = generateJsonPatternFor<UghArray>({ someArray: [123] });
+      class UghArray {
+        someArray?: number[];
+      }
+
+      const generated = generateJsonPatternFor<UghArray>({someArray: [123]});
 
       expect(generated).to.equal(`{ "someArray": [ Number, ... ] OR [], ... }`);
     });
 
     it('containing strings', () => {
-      class UghArray { someArray: string[]; }
-      const generated = generateJsonPatternFor<UghArray>({ someArray: ['abc'] });
+      class UghArray {
+        someArray: string[];
+      }
+
+      const generated = generateJsonPatternFor<UghArray>({someArray: ['abc']});
 
       expect(generated).to.equal(`{ "someArray": [ String, ... ] OR [], ... }`);
     });
 
     it('containing booleans', () => {
-      class UghArray { someArray: boolean[]; }
-      const generated = generateJsonPatternFor<UghArray>({ someArray: [false] });
+      class UghArray {
+        someArray: boolean[];
+      }
+
+      const generated = generateJsonPatternFor<UghArray>({someArray: [false]});
 
       expect(generated).to.equal(`{ "someArray": [ Boolean, ... ] OR [], ... }`);
     });
 
     it('containing objects', () => {
-      class UghArray { someArray: Array<{ something: boolean }>; }
-      const generated = generateJsonPatternFor<UghArray>({ someArray: [{ something: false }] });
+      class UghArray {
+        someArray: Array<{ something: boolean }>;
+      }
+
+      const generated = generateJsonPatternFor<UghArray>({someArray: [{something: false}]});
 
       expect(generated).to.equal(`{ "someArray": [ { "something": Boolean, ... }, ... ] OR [], ... }`);
     });
@@ -100,16 +131,18 @@ describe('generateJsonPatternFor', () => {
       someThingWeDoNotCareAbout?: string;
     }
 
-    const generatedValidPattern = generateJsonPatternFor<Ugh>({
-                                                    someArray: [{ something: true }],
-                                                    someBoolean: false,
-                                                    someEmptyArray: [{ somethingElse: '' }],
-                                                    someNumber: 458,
-                                                    someObject: { someBoolean: true },
-                                                    someString: 'a'
-                                                  });
+    type MockArrayItem = { notValid: boolean } | { something: boolean };
 
-    const mockThing = (someArrayItem) => {
+    const generatedValidPattern = generateJsonPatternFor<Ugh>({
+                                                                someArray: [{something: true}],
+                                                                someBoolean: false,
+                                                                someEmptyArray: [{somethingElse: ''}],
+                                                                someNumber: 458,
+                                                                someObject: {someBoolean: true},
+                                                                someString: 'a'
+                                                              });
+
+    const mockThing = (someArrayItem: MockArrayItem) => {
       const mockObject = {
         someArray: [{}],
         someBoolean: true,
@@ -119,12 +152,13 @@ describe('generateJsonPatternFor', () => {
         someString: 'bah',
         someThingWeDoNotCareAbout: 'still do not care'
       };
+
       mockObject.someArray.unshift(someArrayItem);
       return mockObject;
     };
 
-    const mockThingToValidate = mockThing({ notValid: false });
-    const mockThingWithSomeValidToValidate: Ugh = mockThing({ something: false });
+    const mockThingToValidate = mockThing({notValid: false});
+    const mockThingWithSomeValidToValidate: Ugh = mockThing({something: false});
 
     it('is valid', () => {
       // tslint:disable-next-line:no-unsafe-any
